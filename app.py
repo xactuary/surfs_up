ffc4eca094556c9fa62a359d8a890574aeea695c
xactuary <betsyw@yahoo.com>
2020-11-07 13:46:35 -0500

    final challenge

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import sqlalchemy
+# not declaring a database we are mapping to an already declared database
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+
+# create connection between sql and data
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
+# reflect table to see whats in the database
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
 app = Flask(__name__)
+# root route necessary from http - need to start with this
 @app.route("/")
 def welcome():
     return(
@@ -46,17 +51,17 @@ filter(Measurement.date >= prev_year).all()
     return jsonify(temps=temps)
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
- def stats(start=None, end=None):
-        sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
-
-     if not end: 
-       results = session.query(*sel).\
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
+    if not end: 
+        results = session.query(*sel).\
         filter(Measurement.date <= start).all()
-           temps = list(np.ravel(results))
-           return jsonify(temps)
+        temps = list(np.ravel(results))
+        return jsonify(temps)
 
-     results = session.query(*sel).\
-           filter(Measurement.date >= start).\
-         filter(Measurement.date <= end).all()
-     temps = list(np.ravel(results))
-     return jsonify(temps=temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+ #   app.run() #to run from vscode
---
+++ class.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+# Reflect Tables into SQLAlchemy ORM
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+engine = create_engine("sqlite:///census.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# We can view all of the classes that automap found
+Base.classes.keys()
+
